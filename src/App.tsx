a60999a68d0c2341af0729c22f6d1ab3086daeb8
Paiwast <135978194+Pa1wast@users.noreply.github.com>
2025-01-14 13:42:29 +0300

    Configure shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from './components/ui/button';
+
 function App() {
-  return <div>Jwani Balak CMS</div>;
+  return (
+    <div>
+      Jwani Balak CMS
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
