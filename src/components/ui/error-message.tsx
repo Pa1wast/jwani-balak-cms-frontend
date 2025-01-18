c5394a2c3b3d7027656723cdd33f49d3a896bc07
Paiwast <135978194+Pa1wast@users.noreply.github.com>
2025-01-18 17:04:37 +0300

    Update error-message.tsx

--- src/components/ui/error-message.tsx
+++ src/components/ui/error-message.tsx
@@ -1,5 +1,6 @@
 import { ArrowLeft, Frown } from 'lucide-react';
 import { Button } from './button';
+import { useMoveBack } from '@/hooks/useMoveBack';
 
 interface ErrorMessageProps {
   message: string;
@@ -7,16 +8,20 @@ interface ErrorMessageProps {
 }
 
 function ErrorMessage({ message, goBack = false }: ErrorMessageProps) {
+  const handleGoBack = useMoveBack();
   return (
     <div className="mx-auto w-max flex flex-col gap-2 items-center">
       <p className="text-lg font-semibold flex gap-2 items-center  text-red-500 dark:text-red-500 p-2 bg-destructive/10 dark:bg-red-500/20 w-max rounded-lg">
         <Frown />
         {message}
       </p>
-      <Button variant="ghost" size="sm" className="">
-        <ArrowLeft />
-        Go Back
-      </Button>
+
+      {goBack && (
+        <Button variant="ghost" size="sm" onClick={handleGoBack}>
+          <ArrowLeft />
+          Go Back
+        </Button>
+      )}
     </div>
   );
 }
