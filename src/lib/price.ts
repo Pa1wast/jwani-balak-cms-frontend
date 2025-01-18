cb6031540b6e54817d80938a2260bd38a00028c4
Paiwast <135978194+Pa1wast@users.noreply.github.com>
2025-01-19 01:56:09 +0300

    Update price.ts

--- src/lib/price.ts
+++ src/lib/price.ts
@@ -8,3 +8,15 @@ export function formatPrice(price: number, currency: currencyTypes): string {
 
   return currency === currencyTypes.IQD ? `${formattedPrice} IQD` : `$${formattedPrice}`;
 }
+
+export function formatPercentage(value: number, total: number): string {
+  if (total === 0) {
+    throw new Error('Total must not be zero to avoid division by zero.');
+  }
+
+  const percentage = (value / total) * 100;
+
+  return `${percentage.toFixed(2)}%`;
+}
+
+export function calculateProfit(profitMargin: number, totalCost, sellingPrice) {}
