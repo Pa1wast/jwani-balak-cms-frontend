04c8e86179a4c44729c0fbebd460d9a488f77105
Paiwast <135978194+Pa1wast@users.noreply.github.com>
2025-01-19 16:57:47 +0300

    Update useTransactionsByProductId.ts

--- src/features/transaction/useTransactionsByProductId.ts
+++ src/features/transaction/useTransactionsByProductId.ts
@@ -1,14 +1,14 @@
-import { getTransaction } from '@/api/transaction/get-transaction';
+import { getTransactionsByProductId } from '@/api/transaction/get-transactions-by-product-id';
 import { Transaction } from '@/types/transaction';
 import { useQuery } from '@tanstack/react-query';
 
 export function useTransactionsByProductId(productId: string) {
   const { isLoading, data, error } = useQuery({
-    queryKey: ['transaction'],
-    queryFn: () => getTransaction(productId as string),
+    queryKey: ['transactions'],
+    queryFn: () => getTransactionsByProductId(productId as string),
   });
 
-  const transactions: Transaction[] = data?.data?.transaction;
+  const transactions: Transaction[] = data?.data?.transactions;
 
   return { isLoading, error, transactions };
 }
