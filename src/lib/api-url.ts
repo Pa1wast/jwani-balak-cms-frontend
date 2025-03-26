e862addad006f397d5af92216dd33f19ae56aa75
Paiwast <135978194+Pa1wast@users.noreply.github.com>
2025-03-26 21:42:11 +0300

    fix

--- src/lib/api-url.ts
+++ src/lib/api-url.ts
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://jwani-balak.fairpiranha.box.ca';
-export const API_URL = 'http://localhost:8085';
+export const API_URL = 'https://jwani-balak.fairpiranha.box.ca';
+// export const API_URL = 'http://localhost:8085';
